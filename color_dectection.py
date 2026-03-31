5c9569912fad77083d1ba35d6905e205edcac342
=11525011-code <=>
2026-03-31 16:32:08 +0700

    color detection

---
+++ color_dectection.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+
+def dectect_color(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_red1 = np.array([0, 50, 50])
+    upper_red1 = np.array([10, 255, 255])
+
+    lower_red2 = np.array([170, 100, 100])
+    upper_red2 = np.array([180, 255, 255])
+
+    lower_green = np.array([35, 150, 50])
+    upper_green = np.array([85, 255, 255])
+
+    lower_blue = np.array([100, 150, 50])
+    upper_blue = np.array([140, 255, 255])
+
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask3 = cv2.inRange(hsv, lower_green, upper_green)
+    mask4 = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    mask = mask1 + mask2 + mask3 + mask4
+    cv2.imshow("Combined Mask", mask) # Xem OpenCV có "thấy" cái hộp đỏ không
+    cv2.waitKey(1)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    ## RETR_EXTERNAL, (RETR_TREE, RETR_CCOMP)=>Hierarchy
+    ## Hierachy: [Next, Previous, First_Child, Parent]: Use for complex shape
+    
+    results= [] #color, x, y
+    threshould = 500
+    goal_pos = []
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > threshould:
+            #Centroid
+            M = cv2.moments(cnt)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+
+                label = "UnKnown"
+                if mask1[cY, cX] > 0 or mask2[cY, cX] > 0:
+                    label = "Red"
+                    goal_pos = [0.68, 0,  0.01]
+                elif mask3[cY, cX] > 0:
+                    label = "Green"
+                    goal_pos = [0.48, 0.4,  0.01]
+                elif mask4[cY, cX] > 0:
+                    label = "Blue"
+                    goal_pos = [0.48, -0.4,  0.01]
+                
+                results.append({"color": label, "center": [cX, cY], "area": area, "goal_pos": goal_pos})
+
+                # 3. Vẽ lên frame để quan sát (Debug)
+                cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+                cv2.putText(frame, label, (cX - 20, cY - 20), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                
+        return results, frame
--- test_none_inter.py
+++ test_none_inter.py
@@ -4,17 +4,47 @@ import numpy as np
 import time
 from ik import solve_scara_ik
 from interpolation import interpolate_trapezoidal
+from color_dectection import dectect_color
 import pyautogui
+import cv2
+
+
+
 
 # 1. Load model (Đảm bảo file .xml có khớp type="slide")
 model = mujoco.MjModel.from_xml_path('scene.xml')
 data = mujoco.MjData(model)
-weld_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_EQUALITY, "vaccum_plate")
+
+renderer = mujoco.Renderer(model, height=480, width=480)
+
 
 box_site_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, "bos_pos")
 end_effector = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, "touch_sensor_point")
 box_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, "object_to_pick")
 Goal_point_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, "Goal_point")
+geom_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, "box")
+    
+
+def get_image():
+    mujoco.mj_forward(model, data)
+    renderer.update_scene(data, camera="top_view")
+    image = renderer.render()
+    
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.flip(image, 0)
+    return image
+
+def get_target_for_scara(cX, cY):
+    dx_pixel = cX - 249
+    dy_pixel = cY - 240
+
+    table_size = 0.2
+
+    ratio = table_size / 480
+    obj_x_table = -0.55 - (dy_pixel * ratio)
+    obj_y_table = 0 - (dx_pixel * ratio)
+    return obj_x_table, obj_y_table
+
 
 def reset_goal_pos():
     r_min = 0.4
@@ -34,27 +64,48 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
     viewer.opt.frame = mujoco.mjtFrame.mjFRAME_NONE
     viewer.opt.label = mujoco.mjtLabel.mjLABEL_NONE
     state = -1
-    waiting_duration = 0.5
+    waiting_duration = 0.1
     vacuum_on = False
+    target_obj = None
     release_time = 0
-    print(data.qpos)
+    colors = [
+        [1, 0, 0, 0.5], # Đỏ
+        [0, 1, 0, 0.5], # Xanh lá
+        [0, 0, 1, 0.5]  # Xanh dương
+    ]
 
     
     while viewer.is_running():
+        raw_frame = get_image()
+        output = dectect_color(get_image())
+        if output is not None:
+            results, debug_frame = output
+            cv2.imshow("Camera View", debug_frame)
+        else:
+            # Nếu lỡ bị None, hiển thị khung hình trống hoặc cũ để không crash
+            cv2.imshow("Camera View", raw_frame)
+
+
+        
+   
+        
         print(state, data.time)
+        
         step_start = time.time()
 
-        obj_pos = data.site_xpos[box_site_id]
-        goal_pos = data.xpos[Goal_point_id]
-
         if state == -1:
+            target_obj = None
+            state = 0
 
-            if data.time > waiting_duration:
-                state = 0
-                
-        
-        elif state == 0:
-            q1, q2, q3 = solve_scara_ik(obj_pos[0], obj_pos[1], 0.1) #Giữ độ cao 0.1
+
+        if len(results) > 0 and target_obj is None:
+            target_obj = results[0]
+
+        obj_x, obj_y = get_target_for_scara(target_obj["center"][0], target_obj["center"][1])
+        obj_pos = [obj_x, obj_y, 0.26]
+  
+        if state == 0:
+            q1, q2, q3 = solve_scara_ik(obj_x, obj_y, 0.1) #Giữ độ cao 0.1
             data.ctrl[0], data.ctrl[1], data.ctrl[2] = q1, q2, 0.06
             print(q1, q2)
             #Tính k/c Euclid 
@@ -82,11 +133,17 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                 state = 4
             
         elif state == 4: 
-            q1, q2, q3 = solve_scara_ik(goal_pos[0], goal_pos[1], 0.1) #về Goal
+            
+            goal_x = target_obj["goal_pos"][0]
+            goal_y = target_obj["goal_pos"][1]
+
+
+            q1, q2, q3 = solve_scara_ik(goal_x, goal_y, 0.1) #về Goal
             data.ctrl[0], data.ctrl[1], data.ctrl[2] = q1, q2, 0.06
 
-            goal = np.array([goal_pos[0], goal_pos[1]])
+            goal = np.array([goal_x, goal_y])
             ee_xy = data.site_xpos[end_effector][:2]
+            print(np.linalg.norm(ee_xy - goal))
             if np.linalg.norm(ee_xy - goal) < 0.01:
                 release_time = data.time
                 state = 5
@@ -98,13 +155,11 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                 state = 6
         elif state == 6:
             if data.time - release_time > 0.5:
-                r, theta = reset_goal_pos()
-                x_goal = r * np.cos(theta)
-                y_goal = r * np.sin(theta)
-                data.qpos[10] = x_goal
-                data.qpos[11] = y_goal
-
-                
+                # r, theta = reset_goal_pos()
+                # x_goal = r * np.cos(theta)
+                # y_goal = r * np.sin(theta)
+                # data.qpos[10] = x_goal
+                # data.qpos[11] = y_goal
 
                 x_pos = np.random.uniform(-0.55-0.13, -0.55+0.1)
                 y_pos = np.random.uniform(-0.15, 0.15)
@@ -114,7 +169,11 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                 data.qpos[4] = y_pos
                 data.qpos[5] = 0.26
                 data.qpos[6:10] = quat
-                state = 0
+                
+                random_color = colors[np.random.randint(0, len(colors))]
+                model.geom_rgba[geom_id] = random_color
+
+                state = -1
         
 
 
